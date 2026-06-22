9a65c8c53aa01f043a01c94d95c790fa42570580
Neeraj Sharma <neerajkumarsharma345gmail.com>
2026-06-22 22:05:12 +0530

    refactored FileServiceImpl

--- src/main/java/com/neeraj/assistant/file/service/FileServiceImpl.java
+++ src/main/java/com/neeraj/assistant/file/service/FileServiceImpl.java
@@ -3,6 +3,7 @@ package com.neeraj.assistant.file.service;
 import java.io.IOException;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.neeraj.assistant.common.security.SecurityUtils;
@@ -12,53 +13,87 @@ import com.neeraj.assistant.file.entity.UploadStatus;
 import com.neeraj.assistant.file.repository.FileRepository;
 import com.neeraj.assistant.file.util.FileStorageUtil;
 import com.neeraj.assistant.user.entity.User;
-
 import lombok.RequiredArgsConstructor;
+
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class FileServiceImpl implements FileService {
 
     private final FileRepository fileRepository;
-
     private final FileStorageUtil fileStorageUtil;
 
     @Override
     public FileUploadResponse uploadFile(MultipartFile file) {
 
-    if (file.isEmpty()) {
-        throw new RuntimeException("File is empty");
+        validateFile(file);
+
+        User user = SecurityUtils.getCurrentUser();
+
+        String storedName = storeFile(file);
+
+        FileDocument document = buildFileDocument(file, storedName, user);
+
+        FileDocument savedFile = fileRepository.save(document);
+
+        return buildResponse(savedFile);
     }
 
-    if (!"application/pdf".equals(file.getContentType())) {
+
+
+    private void validateFile(MultipartFile file) {
+
+        if(file.isEmpty()){
+            throw new RuntimeException("File is Empty");
+        }
+
+        if (!"application/pdf".equals(file.getContentType())) {
         throw new RuntimeException("Only PDF files are allowed");
-    }
-    
-    User user = SecurityUtils.getCurrentUser();
-    String storedName;
-    try {
-        storedName = fileStorageUtil.saveFile(file);
-    } catch (IOException e) {
-        throw new RuntimeException("File upload failed", e);
+        }
     }
 
-    FileDocument document =
-        FileDocument.builder()
-                .originalName(file.getOriginalFilename())
-                .storedName(storedName)
-                .contentType(file.getContentType())
-                .size(file.getSize())
-                .filePath("uploads/" + storedName)
-                .status(UploadStatus.UPLOADED)
-                .user(user)
-                .build();
-    FileDocument savedFile = fileRepository.save(document);
+
+
+    private String storeFile(MultipartFile file) {
+
+        try{
+            return fileStorageUtil.saveFile(file);
+        } catch(IOException e){
+            throw new RuntimeException("File Upload failed", e);
+        }
+
+    }
+
+
+
+    private FileDocument buildFileDocument(
+            MultipartFile file,
+            String storedName,
+            User user) {
+
+                return FileDocument.builder()
+                        .originalName(file.getOriginalFilename())
+                        .storedName(storedName)
+                        .contentType(file.getContentType())
+                        .size(file.getSize())
+                        .filePath("uploads/" + storedName)
+                        .status(UploadStatus.UPLOADED)
+                        .user(user)
+                        .build();
+
+    }
+
+
+
+    private FileUploadResponse buildResponse(FileDocument fileDocument) {
+
     return new FileUploadResponse(
-            savedFile.getId(),
-            savedFile.getOriginalName(),
-            savedFile.getStatus(),
-            "File upload succesfully"       
+            fileDocument.getId(),
+            fileDocument.getOriginalName(),
+            fileDocument.getStatus(),
+            "File uploaded successfully"
     );
-   }
 
+    }
 }
