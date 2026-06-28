90a0108ac658026f27cbb2fd2387ddd42b514614
Neeraj Sharma <neerajkumarsharma345gmail.com>
2026-06-28 21:22:54 +0530

    ChatRepo done

---
+++ src/main/java/com/neeraj/assistant/chat/repository/ChatRepository.java
@@ -0,0 +1,26 @@
+package com.neeraj.assistant.chat.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.neeraj.assistant.chat.entity.ChatMessage;
+import com.neeraj.assistant.file.entity.FileDocument;
+import com.neeraj.assistant.user.entity.User;
+
+@Repository
+public interface ChatRepository extends JpaRepository<ChatMessage, UUID>{
+
+    List<ChatMessage> findByFileAndUserOrderByCreatedAtAsc(
+        FileDocument file,
+        User user
+    );
+
+    void deleteByFileAndUser(
+        FileDocument file,
+        User user
+    );
+
+}
